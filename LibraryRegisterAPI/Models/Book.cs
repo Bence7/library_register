4e8f9ba6b6f635ced8fe4701c8229a02952d17ea
Deák Erik <thferik@protonmail.com>
2023-04-25 20:10:59 +0200

    Add Book, Customer, and Rental models

---
+++ LibraryRegisterAPI/Models/Book.cs
@@ -0,0 +1,39 @@
+﻿namespace LibraryRegisterAPI.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Represents a book entity.
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// Gets or sets the ID of the book.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title of the book.
+        /// </summary>
+        [Required]
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the author of the book.
+        /// </summary>
+        [Required]
+        public string? Author { get; set; }
+
+        /// <summary>
+        /// Gets or sets the publisher of the book.
+        /// </summary>
+        [Required]
+        public string? Publisher { get; set; }
+
+        /// <summary>
+        /// Gets or sets the publish date of the book.
+        /// </summary>
+        [Required]
+        public DateTime PublishDate { get; set; }
+    }
+}
