b695d5104493b9d0f62f1e5c15006bb6040a6b31
Jenei Bence László <jeneib13@gmail.com>
2023-06-02 12:40:44 +0200

    Add new member.

---
+++ LibraryAdminApp/Services/AddMember.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Json;
+using LibraryAdminApp.Model;
+
+namespace LibraryAdminApp.Services;
+
+public class AddMember
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _baseUrl;
+    
+    public AddMember(HttpClient httpClient, string baseUrl)
+    {
+        _httpClient = httpClient;
+        _baseUrl = baseUrl;
+    }
+
+    public async void PostMemberAsync(MemberModel memberModel)
+    {
+        await _httpClient.PostAsJsonAsync($"{_baseUrl}/api/addmember", memberModel);
+    }
+}
