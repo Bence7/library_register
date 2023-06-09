146a3d6dc7d4b52fb22eec61f0b79a0fa635657d
Deák Erik <thferik@protonmail.com>
2023-06-09 18:29:36 +0200

    Create LibraryDbContext with entity mappings and DbSet properties

---
+++ LibraryRegisterAPI/LibraryDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LibraryRegisterAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryEntityFramework
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Book { get; set; }
+
+        public virtual DbSet<Member> Member { get; set; }
+
+        public virtual DbSet<Rental> Rental { get; set; }
+    }
+}
