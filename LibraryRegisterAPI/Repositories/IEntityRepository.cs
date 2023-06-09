cca1715432bec05c858b0acab28b87149b6b3277
Deák Erik <thferik@protonmail.com>
2023-06-09 18:38:18 +0200

    Add IEntityRepository interface for generic entity operations

---
+++ LibraryRegisterAPI/Repositories/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using LibraryRegisterAPI.Models;
+
+namespace LibraryRegisterAPI.Repositories
+{
+    public interface IEntityRepository<T>
+    {
+        Task<T> Get(int id);
+
+        Task<IEnumerable<T>> GetAll();
+
+        Task<bool> Add(T entity);
+
+        Task<bool> Update(int id, T entity);
+
+        Task<bool> Delete(int id);
+    }
+}
