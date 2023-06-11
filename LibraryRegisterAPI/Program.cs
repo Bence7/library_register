7e54dcc2b157679bb72567e90370b2a14018edd4
Deák Erik <thferik@protonmail.com>
2023-06-12 01:01:13 +0200

    Added mapper configuration

--- LibraryRegisterAPI/Program.cs
+++ LibraryRegisterAPI/Program.cs
@@ -1,5 +1,5 @@
+using AutoMapper;
 using LibraryEntityFramework;
-using LibraryRegisterAPI.Models;
 using LibraryRegisterAPI;
 using LibraryRegisterAPI.Models.Entities;
 using LibraryRegisterAPI.Repositories;
@@ -10,11 +10,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSqlServer<LibraryDbContext>(builder.Configuration.GetConnectionString("LocalDB"));
+var mapperConfig = new MapperConfiguration(mc =>
+{
+    mc.AddProfile(new MappingProfile());
+});
+
+IMapper mapper = mapperConfig.CreateMapper();
+
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LocalDB")));
+builder.Services.AddSingleton(mapper);
 builder.Services.AddScoped<IEntityRepository<Book>, BookRepository>();
 builder.Services.AddScoped<IEntityRepository<Member>, MemberRepository>();
 builder.Services.AddScoped<IEntityRepository<Rental>, RentalRepository>();
