dbc0ee6f9e4ec2db00b889210c39867ffad83c7f
Jenei Bence László <jeneib13@gmail.com>
2023-06-15 16:02:05 +0200

    Refresh the models.

--- ClassLibrary/Models/MemberModel.cs
+++ ClassLibrary/Models/MemberModel.cs
@@ -1,22 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ClassLibrary.Models;
 
 public class MemberModel
 {
     public int Id { get; set; }
-    
+
     [Required]
     public string? Name { get; set; }
-    
+
     [Required]
     public string? Address { get; set; }
-    
+
     [Required]
     public DateTime BirthDate { get; set; }
-
-    public override string ToString()
-    {
-        return Id + " " + Name + " " + Address + " " + BirthDate;
-    }
 }
